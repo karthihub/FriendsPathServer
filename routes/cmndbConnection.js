979f12fb29937522e7528c2473acb75fb4dcb926
karthihub <karthiikeyansekar@gmail.com>
2018-06-12 10:43:32 +0530

    first commit

--- routes/cmndbConnection.js
+++ routes/cmndbConnection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : '127.0.0.1',
+    host     : 'localhost',
     port     : '3306',
     user     : 'karthivasa',
     password : 'vasa@198karthi',
